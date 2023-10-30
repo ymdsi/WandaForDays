effca1af3764f1fe507b8eac408e6069fa381327
kubo tomoki <oky2284852@stu.o-hara.ac.jp>
2023-10-30 11:03:06 +0900

    Profile Fragment画面作成

--- app/src/main/java/com/example/sinupsample/OtherFragment.java
+++ app/src/main/java/com/example/sinupsample/OtherFragment.java
@@ -1,28 +1,39 @@
 package com.example.sinupsample;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.fragment.app.FragmentTransaction;
 
 public class OtherFragment extends Fragment {
-
-
-
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_other, container, false);
+        View view = inflater.inflate(R.layout.fragment_other, container, false);
 
+        // Profile_button を取得
+        // OtherFragment.java の中で Profile_button のクリックイベントを処理
+        TextView profileButton = view.findViewById(R.id.Profile_button);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ここにクリック時の処理を追加
+                // 例: ProfileFragment に遷移
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, new ProfileFragment());
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+
+        return view;
     }
-
 }
+
